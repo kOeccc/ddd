c22fc5318acfd00f47e2d83ad0ff081886a976a3
kOeccc <koklos1591@gmail.com>
2021-10-28 15:23:19 +0300

    two

--- THIRDWEEK/7per.py
+++ THIRDWEEK/7per.py
@@ -1,1 +1,15 @@
-p = i
+import math
+p = int(input())
+x = int(input())
+y = int(input())
+k = int(input())
+i = 1
+m = 0
+a = 1
+itog = x + y / 100
+while i <= k:
+    a = itog * (1 + p / 100) 
+    i += 1
+    itog = a
+    print(a)
+print(a)
