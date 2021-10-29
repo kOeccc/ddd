adebb4011c7c6ed40510e49e6921e53df534e867
kOeccc <koklos1591@gmail.com>
2021-10-29 20:44:56 +0300

    wd

--- THIRDWEEK/10disc.py
+++ THIRDWEEK/10disc.py
@@ -5,13 +5,9 @@ d = (b * b) - (4 * a * c)
 if d > 0:
     x1 = (-b + (d ** 0.5)) / (2 * a)
     x2 = (-b - (d ** 0.5)) / (2 * a)
-    x1 = int(x1)
-    if x1 < x2:
-        x1 = x1
-    elif x1 > x2:
+    if x1 > x2:
         (x1, x2) = (x2, x1)
-    print(int(x1), int(x2))
-elif int(d) == 0:
+    print(x1, x2)
+elif d == 0:
     x1 = -b / (2 * a)
     print(x1)
-
