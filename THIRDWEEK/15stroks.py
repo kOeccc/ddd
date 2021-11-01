255ebb42e6d10b0f242d509d3be2ca683d130811
kOeccc <koklos1591@gmail.com>
2021-11-01 21:18:53 +0300

    ddd

--- THIRDWEEK/15stroks.py
+++ THIRDWEEK/15stroks.py
@@ -1,6 +1,6 @@
 k = input()
 po = k.find('f')
-po1 = k[::-1].find('f')  # ���_����.find(��� ����)
+po1 = k[::-1].find('f')  # пїЅпїЅпїЅ_пїЅпїЅпїЅпїЅ.find(пїЅпїЅпїЅ пїЅпїЅпїЅпїЅ)
 if k.find('f') != -1:
     if len(k) == po + po1 + 1:
         print(po)
--- THIRDWEEK/16vhod.py
+++ THIRDWEEK/16vhod.py
@@ -1,1 +1,12 @@
 k = input()
+ff = k.find('f')
+i = 0
+while i <= 2:
+    print(ff)
+    ff = k.find('f', ff + 1)
+    i += 1
+    n = ff + k.find('f') - 1
+if i == 1:
+    print('9999')
+elif i == 0:
+    print('-2')
