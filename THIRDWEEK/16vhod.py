5a2cb2d2e5a30828eebada0baace41b44164cab6
kOeccc <koklos1591@gmail.com>
2021-11-04 15:11:56 +0300

    dd

--- THIRDWEEK/16vhod.py
+++ THIRDWEEK/16vhod.py
@@ -1,2 +1,11 @@
 k = input()
-ff = k.find('f')
+po = k.find('f')  # считает с начала строки до буквы ф
+po1 = k[po + 1::]  # делаем строку с найденного символа до конца
+cut = po1.find('f')  # ищем в строке снова ф, уже вторая
+ss = po + cut + 1
+if cut == -1 and po >= 0:
+    print(cut)
+elif cut + po == -2:
+    print('-2')
+elif po < ss:
+    print(ss)
---
+++ THIRDWEEK/17changeplace.py
@@ -0,0 +1,5 @@
+n = input()
+pla = n.find(' ')
+one = n[0:pla + 1:]
+two = n[pla + 1::]
+print(two, one)
