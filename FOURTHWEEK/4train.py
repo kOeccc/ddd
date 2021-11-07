14f74d589fc7d9081d6381a0a8cff35b3ea01206
kOeccc <koklos1591@gmail.com>
2021-11-07 21:50:47 +0300

    d

--- FOURTHWEEK/4train.py
+++ FOURTHWEEK/4train.py
@@ -2,9 +2,9 @@ import math
 
 
 def perm(x1, y1, x2, y2, x3, y3):
-    dist1 = math.sqrt((x2 - x1)**2 + (y2 - y1))**2  # ab
-    dist2 = math.sqrt((x3 - x1)**2 + (y3 - y1))**2  # ca
-    dist3 = math.sqrt((x3 - x2)**2 + (y3 - y2))**2  # bc
+    dist1 = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)  # ab
+    dist2 = math.sqrt((x3 - x1)**2 + (y3 - y1)**2)  # ca
+    dist3 = math.sqrt((x3 - x2)**2 + (y3 - y2)**2)  # bc
     perimetr = dist1 + dist2 + dist3
     return perimetr
 
@@ -15,3 +15,4 @@ x2 = float(input())
 y2 = float(input())
 x3 = float(input())
 y3 = float(input())
+print('{0:.6f}'.format(perm(x1, y1, x2, y2, x3, y3)))
