9b40043fde77fab12f18c8b8422a86df508f3951
kOeccc <koklos1591@gmail.com>
2021-11-09 23:37:01 +0300

    dd

---
+++ FOURTHWEEK/5function.py
@@ -0,0 +1,16 @@
+def sred(a, b):
+    if a < b:
+        return a
+    return b
+
+
+def sred3(a, b, c):
+    return sred(sred(a, b), c)
+
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print(sred(a, b), sred3(a, b, c))
