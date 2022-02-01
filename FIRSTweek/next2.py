9f9dca485a91e22fa7378b023ebc615abcaefe75
kOeccc <koklos1591@gmail.com>
2022-02-01 16:59:16 +0300

    dd

--- FIRSTweek/next2.py
+++ FIRSTweek/next2.py
@@ -1,3 +1,4 @@
 n = int(input())
 k = n % 2
+print(k)
 print((n - k) + 2)
--- FOURTHWEEK/19.py
+++ FOURTHWEEK/19.py
@@ -1,2 +1,12 @@
 def phib(n):
-    
+    # определить две переменные соответсвующие фибо 1 и 1
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
+    else:
+        return phib(n - 1) + phib(n - 2)
+
+
+k = int(input())
+print(phib(k))
